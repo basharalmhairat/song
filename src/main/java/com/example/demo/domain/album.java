c97d42cd9b5eeb70460d4764558efac39e21f984
basharalmhairat <basharalmhairat6y6u@gmail.com>
2022-03-18 18:44:18 +0300

    solved

---
+++ src/main/java/com/example/demo/domain/album.java
@@ -0,0 +1,73 @@
+package com.example.demo.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class album {
+
+    String title;
+    String artist;
+    double songCount;
+    double length;
+    String imageUrl;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+long id;
+
+    public album(){}
+
+    public album(String title, String artist, int songCount, int length, String imageUrl) {
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageUrl =imageUrl;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public double getSongCount() {
+        return songCount;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setSongCount(int songCount) {
+        this.songCount = songCount;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/example/demo/wep/albumRepo.java
@@ -0,0 +1,7 @@
+package com.example.demo.wep;
+
+import com.example.demo.domain.album;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface albumRepo extends JpaRepository<album, Long>{}
+
